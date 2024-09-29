3c56ab96d3b30706eb03cb69301018d8c3103546
AngryCFO <a79955700980@gmail.com>
2024-09-30 00:28:21 +0300

    Updated files

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -1,13 +1,19 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
-import org.junit.jupiter.api.Assertions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Condition.cssClass;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardOrderTest {
+
+    @BeforeAll
+    static void setup() {
+        WebDriverManager.chromedriver().setup(); // Автоматическая настройка ChromeDriver
+    }
 
     private void fillForm(String name, String phone, boolean isAgreementChecked) {
         open("http://localhost:9999/");
@@ -22,27 +28,27 @@ public class CardOrderTest {
     @Test
     void shouldTestForm() {
         fillForm("Иванова Татьяна", "+79001234567", true);
-        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id=order-success]").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
     @Test
     void shouldTestFormDoubleName() {
         fillForm("Иванова-Петрова Татьяна", "+79001234567", true);
-        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id=order-success]").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
     @Test
     void shouldNotTestName() {
         fillForm("Ivanova Татьяна", "+79001234567", true);
         $("[data-test-id=name]").shouldHave(cssClass("input_invalid"));
-        $("[data-test-id=name] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id=name] .input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
     void shouldNotTestPhone() {
         fillForm("Иванова Татьяна", "791234567", true);
         $("[data-test-id=phone]").shouldHave(cssClass("input_invalid"));
-        $("[data-test-id=phone] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $("[data-test-id=phone] .input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
@@ -55,13 +61,13 @@ public class CardOrderTest {
     void shouldNotTestEmptyName() {
         fillForm("", "+79001234567", true);
         $("[data-test-id=name]").shouldHave(cssClass("input_invalid"));
-        $("[data-test-id=name] .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+        $("[data-test-id=name] .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
     }
 
     @Test
     void shouldNotTestEmptyPhone() {
         fillForm("Иванова Татьяна", "", true);
         $("[data-test-id=phone]").shouldHave(cssClass("input_invalid"));
-        $("[data-test-id=phone] .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+        $("[data-test-id=phone] .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
     }
 }
